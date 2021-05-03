3203cae7559bb7cb688badf9193b0b6f7faa81f7
Manuel Fernandez <kiramapers@gmail.com>
2021-05-03 18:29:47 -0500

    comit test

--- src/main/java/com/demonioinka/recargas/DemonioInKrecargaApplication.java
+++ src/main/java/com/demonioinka/recargas/DemonioInKrecargaApplication.java
@@ -8,6 +8,7 @@ public class DemonioInKrecargaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemonioInKrecargaApplication.class, args);
+		System.out.print("trueeeee!");
 	}
 
 }
